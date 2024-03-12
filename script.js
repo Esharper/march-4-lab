f797387fb9897dec6196bfa681e2b13c63684fe9
Esharper <147178117+Esharper@users.noreply.github.com>
2024-03-12 01:14:58 +0000

    added hello

---
+++ script.js
@@ -0,0 +1,10 @@
+
+    // Get the <h1> element
+    const header = document.getElementById('header');
+
+    // Add event listener for mouseover
+    header.addEventListener('mouseover', function() {
+        // Change the color to green
+        header.style.color = 'green';
+    });
+//hello its me again
